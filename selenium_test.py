a8029e0a4f6d1fe1c03fd7116e3e5e689adb85ef
Khosbayar Baatarsaikhan <133148376+Khosbayera@users.noreply.github.com>
2024-04-05 13:23:04 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -13,7 +13,7 @@ driver.get("https://khosbayera.github.io/webpage/")  # Update this with the path
 time.sleep(2)  # Adding a delay to see the result
 
 # Assert some condition to verify the result
-assert "welcome to my web" in driver.title
+assert "My Awesome Web >" in driver.title
 
 # Take a screenshot
 timestamp = time.strftime("%Y%m%d-%H%M%S")
